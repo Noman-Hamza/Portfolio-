62d5e403bd41cf9774f6e290a68cdae2ac9695b1
Noman-Hamza <noman.akmu1@gmail.com>
2025-06-29 08:47:37 +0600

    change routes

--- client/src/component/Service-CRUD.jsx
+++ client/src/component/Service-CRUD.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
-const API_BASE_URL = "https://portfolio-pi-ten-67.vercel.app/api/";
+const API_BASE_URL = "https://portfolio-pi-ten-67.vercel.app/api";
 
 function ServiceCRUD() {
     const navigate = useNavigate();
